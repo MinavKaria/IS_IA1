84cfdb04b563d804c754aefec0399b86df4a3b01
MinavKaria <minavpkaria@gmail.com>
2025-02-09 13:27:47 +0530

    Add changes

--- src/config/arcjet.js
+++ src/config/arcjet.js
@@ -7,7 +7,7 @@ const aj = arcjet({
     shield({ mode: "LIVE" }),
     detectBot({
       mode: "LIVE",
-      allow: [ "CATEGORY:SEARCH_ENGINE"],
+      allow: [ "CATEGORY:SEARCH_ENGINE","CURL"],
     }),
     tokenBucket({
       mode: "LIVE",
